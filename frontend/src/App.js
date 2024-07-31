34a4fd94b089a3ff2e91a4a47503cda04428a377
Kenny Hung (he) <kenny.cfg@outlook.com>
2024-07-31 19:24:05 +0100

    Working FE with button and state variable

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [ message, setMessage ] = useState('Start')
+  
+  const handleButtonClick = () => {
+    setMessage('HELLO WORLD')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>HELLO WORLD</h1>
+      {message && <p>Message is: {message}</p>}
+      <p>Message has got length {message.length}</p>
+      <button onClick={handleButtonClick}>Click me</button>
     </div>
   );
 }
